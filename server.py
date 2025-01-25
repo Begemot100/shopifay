0bad915b0e4008b3532801a973a91f8fc540ad67
Begemot100 <you@example.com>
2025-01-25 07:41:50 +0100

    Initial commit

--- server.py
+++ server.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def home():
     return "Добро пожаловать на сервер Flask!"
 
-@app.route('/location', methods=['POST'])
+@app.route('/location', methods=['POST', 'GET'])
 def location():
-    data = request.json
-    if data:
-        print(f"Координаты: {data}")
-        return jsonify({"status": "success", "message": "Coordinates received"}), 200
-    return jsonify({"status": "error", "message": "No data received"}), 400
+    if request.method == 'GET':
+        return "Этот маршрут поддерживает только POST запросы.", 405
+
+    if request.method == 'POST':
+        data = request.json
+        if data:
+            print(f"Координаты: {data}")
+            return jsonify({"status": "success", "message": "Coordinates received"}), 200
+        return jsonify({"status": "error", "message": "No data received"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
